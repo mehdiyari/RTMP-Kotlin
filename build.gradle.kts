9bb8048f23b2729934222678c994ada77f8e210a
Mehdi Yari <mehdi.email.info@gmail.com>
2022-02-23 15:13:15 +0330

    Init commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.31"
+}
+
+group = "ir.mehdiyari"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
