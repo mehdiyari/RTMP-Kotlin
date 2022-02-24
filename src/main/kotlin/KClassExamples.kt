714af99d831293fc1901ab75d2dfbf3f46d807ca
Mehdi Yari <mehdi.email.info@gmail.com>
2022-02-24 14:06:40 +0330

    Add more examples for using KClass

--- src/main/kotlin/KClassExamples.kt
+++ src/main/kotlin/KClassExamples.kt
@@ -1,6 +1,33 @@
 import kotlin.reflect.KClass
+import kotlin.reflect.KVisibility
+import kotlin.reflect.full.createInstance
 
 fun main() {
+
+    // Create an instance from Person class with reflection
+    val person = Person::class.createInstance()
+    println("Person name is ${person.name}")
+
+
+    // Checking Person class is data class or not
+    if (Person::class.isData) {
+        println("Person is an data class")
+    }
+
+    // Checking Person class has internal modifier or not
+    if (Person::class.visibility == KVisibility.INTERNAL) {
+        println("Person class has internal modifier")
+    }
+
+    // Checking Person class is open or final
+    if (Person::class.isOpen) {
+        println("Person class has open modifier")
+    } else {
+        println("Person class hasn't open modifier")
+    }
+
+    println("----------------")
+
     printClassConstructors(Person::class)
 }
 
--- src/main/kotlin/Person.kt
+++ src/main/kotlin/Person.kt
@@ -1,7 +1,9 @@
-data class Person constructor(
+internal data class Person constructor(
     val name: String,
     val age: Int
 ) {
     @Deprecated("Deprecated Constructor")
     constructor(name: String) : this(name, 1)
+
+    constructor(): this("RMS", 68)
 }
